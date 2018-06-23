bac29af5e56e1bbd2856f4af7a89851dce7a96cd
asv7780 <asv7780@gmail.com>
2018-06-23 23:26:54 +0300

    First commit

---
+++ src/app/countdown/countdown/countdown.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-countdown',
+  templateUrl: './countdown.component.html',
+  styleUrls: ['./countdown.component.css']
+})
+export class CountdownComponent implements OnInit {
+  date = new Date();
+  delta: any;
+  status: string;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.calculateSummerCounter();
+  }
+  calculateSummerCounter() {
+    const currentYear = this.date.getFullYear();
+    const currentMonth = this.date.getMonth();
+    let startSummer = new Date(currentYear, 5, 1);
+
+
+    if (currentMonth >= 5 && currentMonth < 8) {
+      this.status = 'Сейчас лето';
+    } else if (currentMonth >= 8) {
+      startSummer = new Date(currentYear + 1, 5, 1);
+      this.delta = +startSummer - +this.date;
+      this.status = `До лета осталось ${this.delta} дней.`;
+    } else {
+      this.delta = Math.abs((+this.date - (+startSummer)) / (24 * 3600 * 1000));
+      this.status = `До лета осталось ${parseInt(this.delta, 10)} дней.`;
+    }
+
+  }
+}
